2b5463dbbf0cf09a7f344ed9a379db5947c8e076
alexaizq <65555006+alexaizq@users.noreply.github.com>
2025-04-14 12:02:11 -0500

    gps

--- sw/accelerometer.py
+++ sw/accelerometer.py
@@ -6,7 +6,6 @@ import adafruit_adxl34x
 # Create I2C connection
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
-accelerometer.set_range(64)
 accelerometer.set_data_rate(100)  # Set data rate to 100 Hz
 
 def getAcceleration():
