3308200416bfdbdde173315af03b8e4b730c165c
alexaizq <65555006+alexaizq@users.noreply.github.com>
2025-04-19 20:18:19 -0500

    Add files via upload

--- sw/loadcell.py
+++ sw/loadcell.py
@@ -1,21 +1,46 @@
-# script with functions to interface with load cell and amplifier board hx711
+# example code from https://github.com/mpibpc-mroose/hx711/ adapted to read 50 kg load cells for paraglider
+# last modified: 4/19/25
+# author: Alexa
 
-# reading raw data from hx711
+
+#!/usr/bin/python3
+from hx711 import HX711		# import the class HX711
+import RPi.GPIO as GPIO		# import GPIO
 import time
-import board
-import busio
-import adafruit_hx711
-import numpy as np
 
-def getLoadCellData():
-    # Create I2C connection
-    i2c = busio.I2C(board.SCL, board.SDA)
-    hx711 = adafruit_hx711.HX711(i2c, 0x1E, 0x1F)  # Use the correct I2C address for your HX711
+try:
+    DataPin = 6
+    ClockPin = 5
+    NumReadings = 1
 
-    # Set the scale factor (calibration factor) for the load cell
-    hx711.set_scale(2280.0)  # Adjust this value based on your calibration
+    print("Reading HX711")
+    # Create an object hx which represents your real hx711 chip
+    # Required input parameters are only 'dout_pin' (data) and 'pd_sck_pin' (clock)
+    # If you do not pass any argument 'gain_channel_A' then the default value is 128
+    # If you do not pass any argument 'set_channel' then the default value is 'A'
+    # you can set a gain for channel A even though you want to currently select channel B
+    hx = HX711(dout_pin=DataPin, pd_sck_pin=ClockPin, gain=128, channel='A')
+    
+    print("Reset")
+    result = hx.reset()		# Before we start, reset the hx711 ( not necessary)
+    if result:			# you can check if the reset was successful
+        print('Ready to use')
+    else:
+        print('not ready')
+        
+    while True:
+        # R ead data several, or only one, time and return mean value
+        # it just returns exactly the number which hx711 sends
+        # argument times is not required default value is 1
+        data = hx._read()
+        
+        if data != False:	# always check if you get correct value or only False
+            print('Raw data: ' + str(data))
+        else:
+            print('invalid data')               
 
-    # Read raw data from the load cell
-    raw_data = hx711.read_raw()
-    return raw_data
-
+except (KeyboardInterrupt, SystemExit):
+    print('Bye :)')
+    
+finally:
+    GPIO.cleanup()
